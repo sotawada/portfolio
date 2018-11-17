6a3e09b5f370806061874082600c17e4aa6c2959
Sota Wada <sota@MacBook-Pro-5.local>
2018-11-18 01:42:42 +0900

    setup the register

---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'portfolio',
+    user: process.env.DB_USER || 'portfolio',
+    password: process.env.DB_PASS || 'portfolio',
+    options: {
+      dialect: process.env.DIALECT || 'sqlite',
+      host: process.env.HOST || 'localhost',
+      storage: './portfolio.sqlite'
+    }
+  }
+}
